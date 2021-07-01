3a3d13e48c61a71f14b997ad787fc9aebf9fa464
yuvalc7 <ycyuvalc@gmail.com>
2021-07-01 08:22:03 +0300

    commit2

--- server.js
+++ server.js
@@ -1,29 +1,9 @@
 const express = require('express')
 const port  = process.env.PORT || 8080
-const path = require("path")
+
 const app = express()
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-        res.send(200);
-    }
-    else {
-        next();
-    }
-};
-
-app.configure(function () {
-    app.use(allowCrossDomain);
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
 
 app.use(express.static(__dirname + '/dist/'));
 
