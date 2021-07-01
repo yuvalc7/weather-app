32c89e7d8ff45ed92fa085d0ceecb2392e5f4cce
yuvalc7 <ycyuvalc@gmail.com>
2021-07-01 09:01:10 +0300

    commit9

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default function apiRequest(path, key){
 
-    return axios.get('http://dataservice.accuweather.com/' + path, {
+    return axios.get(process.env.VUE_APP_ACCU_WEATHER_ENDPOINT + path, {
             params: {
                 apikey: process.env.VUE_APP_API_KEY,
                 ...(key ? { q: key } : {})
