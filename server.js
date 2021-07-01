c255226b63da21547e85ded1d9514c73858d68d4
yuvalc7 <ycyuvalc@gmail.com>
2021-07-01 08:30:55 +0300

    commit3

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const port  = process.env.PORT || 8080
-
+const cors = require('cors');
 const app = express()
 
-
-
+app.use(cors());
 app.use(express.static(__dirname + '/dist/'));
 
 app.get(/.*/, function (req,res){
